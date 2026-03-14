1734a7613840cb65b91467660e9caaad13bf6653
ahmedb0o0di <a7med.sa3ed8@gmail.com>
2026-03-15 00:45:26 +0200

    feat: add Home link to Burger Menu

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -40,6 +40,16 @@ const navigate = (path) => {
         </div>
         
         <div class="menu-content">
+          <ul class="menu-list">
+            <li class="menu-item" @click="navigate('/')">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <polyline points="9 22 9 12 15 12 15 22"></polyline>
+              </svg>
+              Home
+            </li>
+          </ul>
+
           <div class="menu-label">Categories</div>
           <ul class="menu-list">
             <li class="menu-item" v-for="cat in categories" :key="cat.id" @click="navigate(`/category/${cat.id}`)">
